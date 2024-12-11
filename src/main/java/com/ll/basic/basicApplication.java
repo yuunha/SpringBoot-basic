e0bd48b3bfe7d8cb39a923f002a9f153f1e8dbe0
yuunha <h000414@naver.com>
2024-12-11 10:42:28 +0900

    setting

---
+++ src/main/java/com/ll/basic/basicApplication.java
@@ -0,0 +1,13 @@
+package com.ll.basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class basicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(basicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/basic/SpringBasic20241211ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBasic20241211ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
